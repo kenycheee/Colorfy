ca37682c39bb2ecfefd7914c14317d214b92b5c5
kenycheee <khenichi.k@gmail.com>
2025-11-17 19:24:15 +0700

    refactor: remove unnecessary comments from firebase config

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";;
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBhnhEpPCuabwDFad74PyB-X7zSr4-27p0",
   authDomain: "colorfy-535230.firebaseapp.com",
@@ -16,7 +11,6 @@ const firebaseConfig = {
   measurementId: "G-DEFSFBGGQW"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
